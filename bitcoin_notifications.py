bb2486080da77e734ef90ab0e0c729b2efb6b237
jnels13 <jnels13@gmail.com>
2021-06-16 00:45:56 -0400

    pulled BTC exchange rate

--- bitcoin_notifications.py
+++ bitcoin_notifications.py
@@ -1,5 +1,14 @@
-import requests
-bitcoin_api_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+# Project from https://realpython.com/python-bitcoin-ifttt/
+import requests, os
+
+#Load key
+working_dir = os.getcwd()
+with open(working_dir+"/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/alpha_api_key.txt", 'r') as file:
+    alpha_key = file.read()
+
+#Request exchange rate BTC to USD
+bitcoin_api_url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey="+alpha_key
 response = requests.get(bitcoin_api_url)
 response_json = response.json()
-print(response_json)
+cur_btc_value = response_json['Realtime Currency Exchange Rate']['5. Exchange Rate']
+
