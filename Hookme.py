eaf5c3e3028baf137411781fda642639c125d11b
jnels13 <jnels13@gmail.com>
2021-06-17 00:26:10 -0400

    remove key

--- Hookme.py
+++ Hookme.py
@@ -1,5 +1,9 @@
 #Send webhook
-import requests
+import requests, os
 
-zap_webhook_url = #fill me in
+working_dir = os.getcwd()
+with open(working_dir+"/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/zapier_key_url.txt", 'r') as file:
+    zap_key = file.read()
+
+zap_webhook_url = zap_key
 requests.post(zap_webhook_url)
