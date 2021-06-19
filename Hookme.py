fef003f6e66c46449bb2b120e735f6b8dc184fbb
jnels13 <jnels13@gmail.com>
2021-06-19 11:29:37 -0400

    updates to hook

--- Hookme.py
+++ Hookme.py
@@ -1,9 +1,11 @@
-#Send webhook
+
 import requests, os
 
+# Pull the Zapier key
 working_dir = os.getcwd()
 with open(working_dir+"/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/zapier_key_url.txt", 'r') as file:
     zap_key = file.read()
 
+    # Request webhook previously set up on Zapier
 zap_webhook_url = zap_key
 requests.post(zap_webhook_url)
--- bitcoin_notifications.py
+++ bitcoin_notifications.py
@@ -6,9 +6,11 @@ working_dir = os.getcwd()
 with open(working_dir+"/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/alpha_api_key.txt", 'r') as file:
     alpha_key = file.read()
 
-#Request exchange rate BTC to USD
+#Request exchange rate Bitcoin to US Dollars
 bitcoin_api_url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey="+alpha_key
 response = requests.get(bitcoin_api_url)
 response_json = response.json()
 cur_btc_value = response_json['Realtime Currency Exchange Rate']['5. Exchange Rate']
+
+
 
