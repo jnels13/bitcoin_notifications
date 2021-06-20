b31119ea54df5824ddb349212985ee7571877fce
jnels13 <jnels13@gmail.com>
2021-06-20 14:02:18 -0400

    adding functions

--- bitcoin_notifications.py
+++ bitcoin_notifications.py
@@ -1,16 +1,40 @@
 # Project from https://realpython.com/python-bitcoin-ifttt/
 import requests, os
 
-#Load key
+# Pull the Zapier key
+working_dir = os.getcwd()
+with open(working_dir+"/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/zapier_key_url.txt", 'r') as file:
+    zap_key = file.read()
+
+#Load alphavantage key and save URL
 working_dir = os.getcwd()
 with open(working_dir+"/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/alpha_api_key.txt", 'r') as file:
     alpha_key = file.read()
-
-#Request exchange rate Bitcoin to US Dollars
 bitcoin_api_url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey="+alpha_key
-response = requests.get(bitcoin_api_url)
-response_json = response.json()
-cur_btc_value = response_json['Realtime Currency Exchange Rate']['5. Exchange Rate']
+
+
+def get_latest_bitcoin_price():
+    """
+    Requests latest bitcoin-USD exchange rate from
+    alphavantage.com.  No arguments, returns a 2-
+    decimal place float.
+    """
+    response = requests.get(bitcoin_api_url)
+    response_json = response.json()
+    cur_btc_value = float(response_json['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+    return(round(cur_btc_value, 2))
+
+
+# Request webhook previously set up on Zapier
+zap_webhook_url = zap_key
+requests.post(zap_webhook_url)
+
+
+def __main__(): 
+    pass
+
+if __name__ == "__main__":
+    main()
 
 
 
---
+++ btc_price.py
@@ -0,0 +1,13 @@
+import requests, os
+
+#Load alphavantage key and save URL
+working_dir = os.getcwd()
+with open(working_dir+"/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/alpha_api_key.txt", 'r') as file:
+    alpha_key = file.read()
+bitcoin_api_url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey="+alpha_key
+
+response = requests.get(bitcoin_api_url)
+response_json = response.json()
+cur_btc_value = round(float(response_json['Realtime Currency Exchange Rate']['5. Exchange Rate']),2)
+
+print(cur_btc_value)
