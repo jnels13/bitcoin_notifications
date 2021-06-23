b330eaaaba14c6e041e904f8ef7b9eac83f31fad
jnels13 <jnels13@gmail.com>
2021-06-22 23:11:43 -0400

    issues w zapier

--- Hookme.py
+++ Hookme.py
@@ -3,9 +3,21 @@ import requests, os
 
 # Pull the Zapier key
 working_dir = os.getcwd()
+payload = {
+            "contact": 
+                {"name":"joe",
+                 "phone":"123-456-7890",
+                 "address":"who cares"},
+            "information":
+                {"date":"12-23-45",
+                 "time":"23:12",
+                 "subject":"no subject"}
+          }
 with open(working_dir+"/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/zapier_key_url.txt", 'r') as file:
     zap_key = file.read()
 
     # Request webhook previously set up on Zapier
 zap_webhook_url = zap_key
-requests.post(zap_webhook_url)
+
+test_webhook = "https://webhook.site/dd566286-7ea1-4caf-bb7f-3b97d3f25b3b"
+requests.post(test_webhook, data=payload)
--- bitcoin_notifications.py
+++ bitcoin_notifications.py
@@ -6,6 +6,10 @@ working_dir = os.getcwd()
 with open(working_dir+"/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/zapier_key_url.txt", 'r') as file:
     zap_key = file.read()
 
+# Request webhook previously set up on Zapier
+zap_webhook_url = zap_key
+#requests.post(zap_webhook_url)
+
 #Load alphavantage key and save URL
 working_dir = os.getcwd()
 with open(working_dir+"/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/alpha_api_key.txt", 'r') as file:
@@ -24,6 +28,11 @@ def get_latest_bitcoin_price():
     cur_btc_value = float(response_json['Realtime Currency Exchange Rate']['5. Exchange Rate'])
     return(round(cur_btc_value, 2))
 
+def post_webhook (event, value):
+    # payload sent to Zapier
+    data = {"value1": value}
+    #inserts desired event
+    zap_event_url = zap_webhook_url + "/" + event
 
 # Request webhook previously set up on Zapier
 zap_webhook_url = zap_key
