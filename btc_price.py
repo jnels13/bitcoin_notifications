deb475d2514608007e6b6924b967c78098fadebe
jnels13 <jnels13@gmail.com>
2021-06-24 23:58:59 -0400

    moving to Twilio

--- btc_price.py
+++ btc_price.py
@@ -2,6 +2,7 @@ import requests, os
 
 #Load alphavantage key and save URL
 working_dir = os.getcwd()
+print(working_dir)
 with open(working_dir+"/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/alpha_api_key.txt", 'r') as file:
     alpha_key = file.read()
 bitcoin_api_url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey="+alpha_key
