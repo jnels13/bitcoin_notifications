c4fff6dd6f9db8124d43abb823591c1b2e2d658f
jnels13 <jnels13@gmail.com>
2021-06-25 00:20:12 -0400

    updated to use sms via twilio

--- bitcoin_notifications.py
+++ bitcoin_notifications.py
@@ -1,16 +1,20 @@
 # Project from https://realpython.com/python-bitcoin-ifttt/
 import requests, os, time
+from twilio.rest import Client
 from datetime import datetime
 
-# Pull the IFTTT key and insert into URL
-with open("/Users/joenelson/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/ifttt_key.txt", 'r') as file:
-    ifttt_key = file.read()
-ifttt_webhook_url = "https://maker.ifttt.com/trigger/{}/with/key/"+ifttt_key
-print(ifttt_webhook_url)
 #Load alphavantage key and save URL
 with open("/Users/joenelson/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/alpha_api_key.txt", 'r') as file:
     alpha_key = file.read()
 bitcoin_api_url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey="+alpha_key
+
+# Import SMS tokens/numbers
+with open("/Users/joenelson/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/twilio_id_token.txt", 'r') as file:
+    twilio_key = file.read()
+with open("/Users/joenelson/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/twilio_account_sid.txt", 'r') as file:
+    twilio_sid = file.read()
+with open("/Users/joenelson/Documents/Python_Projects/Bitcoin_Notifications/bitcoin_notifications/my_number.txt", 'r') as file:
+    my_number = file.read()
 
 
 def get_latest_bitcoin_price():
@@ -19,32 +23,39 @@ def get_latest_bitcoin_price():
     alphavantage.com.  No arguments, returns a 2-
     decimal place float.
     """
-
     response = requests.get(bitcoin_api_url)
     response_json = response.json()
     cur_btc_value = float(response_json['Realtime Currency Exchange Rate']['5. Exchange Rate'])
     return(round(cur_btc_value, 2))
 
-def post_webhook (event, value):
-    payload = {"Value1": value}
-    #inserts desired event
-    ifttt_event_url = ifttt_webhook_url.format(event)
-    #send http post to webhook URL
-    requests.post(ifttt_event_url, data=payload)
-    print(payload)
+def send_sms(message, to_number=my_number):
+    account_sid = twilio_sid
+    auth_token  = twilio_key
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        to=to_number, 
+        from_="+12028001098",
+        body=message)
+    print(message.sid)
 
-bc_price_threshold = 10000
+bc_price_threshold = 35500
+change_threshold = 0.001
+sleep_time = 10
 
 def main(): 
 
+    price = get_latest_bitcoin_price()
+    message = "Bitcoin price is $"+str(price)
+    send_sms(message)
+    time.sleep(sleep_time)
+
     while True:
-        price = get_latest_bitcoin_price()
-        print(price)
-
-        if float(price) > bc_price_threshold:
-            post_webhook('bitcoin_price', price)
-
-        time.sleep(30)
+        new_price = get_latest_bitcoin_price()
+        percent_change = abs((new_price-price)/price)
+        if percent_change > change_threshold:
+            message("Bitcoin price change of "+str(percent_change)+"%, now at $"+str(new_price))
+            send_sms(message)
+        time.sleep(sleep_time)
 
 if __name__ == "__main__":
     main()
